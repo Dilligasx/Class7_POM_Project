c12167e59230070e500d2a0c48560e5868e71a60
Dilligasx <aa.zia26@gmail.com>
2022-04-15 20:27:31 -0500

    class7_commit

--- src/main/java/util/BrowserFactory.java
+++ src/main/java/util/BrowserFactory.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class BrowserFactory {
 
